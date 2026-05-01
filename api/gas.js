26551585c6cec8a8b3ca5bcc64359fce9c580037
ZabirovRinat <zabirovavetis@gmail.com>
2026-05-01 23:40:35 +0500

    Update gas.js

--- api/gas.js
+++ api/gas.js
@@ -1,4 +1,3 @@
-// api/gas.js
 export default async function handler(req, res) {
   if (req.method === 'OPTIONS') {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -7,12 +6,10 @@ export default async function handler(req, res) {
     return res.status(200).end();
   }
 
-  // URL вашего Google Apps Script (тот самый, который сейчас прописан в GAS_URL)
-  const GAS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbymCZRkr5i12vox8xfbdMrXileNMLMYAWOg3a69SwASH1wV0Sug620fo0GZXgQ1A2a-8A/exec';
+  const GAS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyQluQxEYnKJ0i6aLiZJ93VzEoyDsj8GYUdH_Jmjg9NIlDuFAoQaLQljH1lZQoRasaRvA/exec';
 
   try {
     const url = new URL(GAS_SCRIPT_URL);
-    // Прокидываем query-параметры для GET
     if (req.method === 'GET') {
       for (const [key, value] of Object.entries(req.query)) {
         url.searchParams.set(key, value);
@@ -21,9 +18,7 @@ export default async function handler(req, res) {
 
     const response = await fetch(url.toString(), {
       method: req.method,
-      headers: {
-        'Content-Type': req.headers['content-type'] || 'application/json',
-      },
+      headers: { 'Content-Type': req.headers['content-type'] || 'application/json' },
       body: req.method === 'POST' ? JSON.stringify(req.body) : undefined,
     });
 
